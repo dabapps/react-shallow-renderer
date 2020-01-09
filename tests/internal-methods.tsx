cdd33160bc29b001dcb7a36efc627fd146d4f456
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-01-09 16:43:58 +0000

    Manualy format the files

--- tests/internal-methods.tsx
+++ tests/internal-methods.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { ReactAnyNode, ReactShallowRenderer } from '../src';
 
 describe('ReactShallowRenderer', () => {
--- tests/to-json/forward-ref.tsx
+++ tests/to-json/forward-ref.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { ReactShallowRenderer } from '../../src';
 import { elementSymbol } from '../../src/constants';
 import { compare } from '../helpers/compare';
